618e139b0780371898fa8cab57ef57c1c500dfe3
yeswanth-2003 <chepuriyeswanth27@gmail.com>
2025-01-06 12:41:56 +0530

    speed insights

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights/>
  
   </React.StrictMode>
 );
